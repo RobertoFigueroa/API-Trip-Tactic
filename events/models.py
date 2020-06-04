2e4d3ccd3e22140c28f5b36ae72c4cbc3230a8a4
RobertoFigueroa <fig18306@uvg.edu.gt>
2020-06-04 09:58:10 -0600

    hour field format change

--- events/models.py
+++ events/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
-    hour = models.TimeField(auto_now=False, auto_now_add=False)
+    hour = models.CharField(max_length=30)
     description = models.CharField(max_length=2000)
     place = models.ForeignKey(
         'places.Place',
