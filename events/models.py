298ba7e93228fc45323e3c8fbc71a97d783d2bb3
RobertoFigueroa <fig18306@uvg.edu.gt>
2020-06-04 09:30:52 -0600

    Fixed bugs in events

--- events/models.py
+++ events/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
-
     description = models.CharField(max_length=2000)
     place = models.ForeignKey(
         'places.Place',
